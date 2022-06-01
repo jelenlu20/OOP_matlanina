8e3c3b4f93f6af4ba9977b342113b40d48e9c51c
LYX86 <100358987+jelenlu20@users.noreply.github.com>
2022-06-01 20:20:06 +0200

    minecraft

---
+++ minecraft_server.py
@@ -0,0 +1,38 @@
+
+class Hrac:
+    """
+    hráč minecraftu
+    """
+
+    def __init__(self, jmeno):
+        self.jmeno = _jmeno
+
+
+
+class Inventar:
+    """
+    hráčův inventář/data hráče
+    """
+
+    def __init__(self):
+        pass
+
+
+
+class Server:
+    """
+    minecraft server
+    """
+
+    def __init__(self, nazev):
+        self.nazev = _nazev
+
+
+
+class Svet:
+    """
+    svět na servru
+    """
+
+    def __init__(self, nazev):
+        self.nazev = _nazev
